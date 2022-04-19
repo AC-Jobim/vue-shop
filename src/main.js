e284f7996cb054ed48ab1764a743d3536dabce8c
zhaobin <1306462280@qq.com>
2022-04-19 11:13:27 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -34,6 +34,31 @@ axios.interceptors.request.use(config => {
 })
 
 
+// 引入Quill
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+
+
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+
 Vue.config.productionTip = false
 
 new Vue({
